7106c2d79955947670f50a38a6ca9b4378116ec1
Danlois Tovar <danlois.tovar@gmail.com>
2023-12-14 20:17:51 -0300

    Se agrega tailwindcss.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './assets/styles/App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className="text-4xl font-bold text-indigo-500">App Gift</h1>
+        </>
+    );
+}
+
+export default App;
