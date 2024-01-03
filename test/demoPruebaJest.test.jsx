52c4a5fd1d0441787e43d1746dfe3d9507860dcd
Danlois Tovar <danlois.tovar@gmail.com>
2024-01-03 19:35:07 -0300

    Se prueba de error de husky.

--- test/demoPruebaJest.test.jsx
+++ test/demoPruebaJest.test.jsx
@@ -3,6 +3,12 @@ describe('Prueba de ambiente de Jest!', () => {
 		console.log('Corriendo test de prueba!');
 	});
 
-	test('should', () => {
-		
+	// Comprobar que un número es mayor a cero
+	const isPositive = (n) => n > 0;
+	test('isPositive should return true when the value is positive', () => {
+		expect(isPositive(-1)).toBeTruthy(); // El resultado se
+		// comprueba con toBeTruth
+		// y devuelve true si el valor es verdadero, false en caso contr
+		//ario
+	});
 });
