fdc1e3d5a8bafa6fd002e435d5bc5f647cc44ad1
Danlois Tovar <danlois.tovar@gmail.com>
2024-01-03 20:18:03 -0300

    Prueba de eslint es husky.

--- test/demoPruebaJest.test.jsx
+++ test/demoPruebaJest.test.jsx
@@ -1,12 +1,17 @@
 describe('Prueba de ambiente de Jest!', () => {
-	test('should', () => {
+	test('Prueba', () => {
+		function add(a, b) {
+			return a + b;
+		}
 		console.log('Corriendo test de prueba!');
+
+		expect(add(1, 2)).toBe(4);
 	});
 
 	// Comprobar que un número es mayor a cero
 	const isPositive = (n) => n > 0;
 	test('isPositive should return true when the value is positive', () => {
-		expect(isPositive(-1)).toBeTruthy(); // El resultado se
+		expect(isPositive(1)).toBeTruthy(); // El resultado se
 		// comprueba con toBeTruth
 		// y devuelve true si el valor es verdadero, false en caso contr
 		//ario
